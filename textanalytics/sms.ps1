e77c062530826702f1ca9a0c0427a4a3b0bc2592
WaltonChiang <waltonchiang@microsoft.com>
2023-06-20 16:28:08 -0500

    remove cred

--- textanalytics/sms.ps1
+++ textanalytics/sms.ps1
@@ -2,15 +2,15 @@
 
 $context = Get-Content -Path .\output.txt -Raw
 
-$url = "https://api.twilio.com/2010-04-01/Accounts/ACbd0d26ef725cb220bf6e68d3fe4f9870/Messages.json"
+$url = "https://api.twilio.com/2010-04-01/Accounts/[accountid]/Messages.json"
 $params = @{
-  To = "+14159881905"
-  From = "+18449942627"
+  To = "[mynumber]"
+  From = "[secret]"
   Body = $context
 }
-$secret = "74ca84b32c05f9c9a2ae62c45d8b4f17" |
+$secret = "[api key]" |
 ConvertTo-SecureString -asPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential("ACbd0d26ef725cb220bf6e68d3fe4f9870", $secret)
+$credential = New-Object System.Management.Automation.PSCredential("[accountid]", $secret)
 Invoke-WebRequest $url -Method Post -Credential $credential -Body $params -UseBasicParsing | Out-Null
 
 # delete output.txt
